4bdd672a3a20a734b0edad02f1f0338e25e8a9a8
teodoramaximiuc <teodora.maximiuc@endava.com>
2025-09-01 13:53:14 +0300

    kubernetes + health

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.get("/health")
+def health():
+    return "OK", 200
+
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 5000)
     
